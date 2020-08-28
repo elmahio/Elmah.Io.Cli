d47dbf5a2cab0c0c1c9dc8bdbe33a0b98352ace7
Thomas Ardal <thomasardal@gmail.com>
2020-08-28 12:26:50 +0200

    Expand date range to get messages logged around the time where last looked + console colors

--- src/Elmah.Io.Cli/TailCommand.cs
+++ src/Elmah.Io.Cli/TailCommand.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Text;
+using System.Linq;
 using System.Threading;
 
 namespace Elmah.Io.Cli
@@ -29,36 +29,83 @@ namespace Elmah.Io.Cli
             {
                 var api = Api(apiKey);
                 var from = DateTime.UtcNow;
+                var previous = new List<string>();
                 while (true)
                 {
                     Thread.Sleep(5000);
-                    var newNow = DateTime.UtcNow;
-                    var result = api.Messages.GetAll(logId.ToString(), 0, 0, "*", from, newNow, false);
+                    var now = DateTime.UtcNow;
+                    var fiveSecondsBefore = from.AddSeconds(-5);
+                    var result = api.Messages.GetAll(logId.ToString(), 0, 0, "*", fiveSecondsBefore, now, false);
                     if (result?.Total.HasValue != true || result.Total.Value == 0)
                     {
-                        from = newNow;
+                        from = now;
+                        previous.Clear();
                         continue;
                     };
 
                     int total = result.Total.Value;
                     int i = 0;
+                    var messages = new List<MessageOverview>();
                     while (i < total)
                     {
-                        var respons = api.Messages.GetAll(logId.ToString(), i / 10, 10, "*", from, newNow, false);
-                        foreach (MessageOverview message in respons.Messages)
-                        {
-                            Console.WriteLine($"{message.DateTime.Value}|{message.Severity}|{message.Title}");
-                            i++;
-                        }
+                        var response = api.Messages.GetAll(logId.ToString(), i / 10, 10, "*", fiveSecondsBefore, now, false);
+                        messages.AddRange(response.Messages.Where(msg => !previous.Contains(msg.Id)));
+                        i += response.Messages.Count;
                     }
 
-                    from = newNow;
+                    previous.Clear();
+
+                    foreach (var message in messages.OrderBy(msg => msg.DateTime.Value))
+                    {
+                        SetColor(message.Severity);
+                        Console.WriteLine($"{message.DateTime.Value}|{message.Severity}|{message.Title}");
+                        previous.Add(message.Id);
+                        ResetColor();
+                    }
+
+                    from = now;
                 }
 
                 return 0;
             });
 
             return logCommand;
+        }
+
+        private static void SetColor(string severity)
+        {
+            switch(severity)
+            {
+                case "Verbose":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "Debug":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "Information":
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "Warning":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "Error":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "Fatal":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+
+        private static void ResetColor()
+        {
+            Console.ResetColor();
         }
     }
 }
