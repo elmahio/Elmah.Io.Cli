dd11ef07f770c3eae3121540eec977a4305baa5d
Thomas Ardal <thomasardal@gmail.com>
2023-03-10 12:08:35 +0100

    Output errors from the elmah.io API

--- src/Elmah.Io.Cli/CommandBase.cs
+++ src/Elmah.Io.Cli/CommandBase.cs
@@ -1,4 +1,5 @@
 ﻿using Elmah.Io.Client;
+using Spectre.Console;
 using System;
 using System.Net.Http.Headers;
 
@@ -15,7 +16,13 @@ namespace Elmah.Io.Cli
                 Timeout = new TimeSpan(0, 1, 0),
                 UserAgent = new ProductInfoHeaderValue(new ProductHeaderValue("Elmah.Io.Cli", _assemblyVersion)).ToString(),
             });
+            api.Messages.OnMessageFail += Messages_OnMessageFail;
             return api;
+        }
+
+        private static void Messages_OnMessageFail(object sender, FailEventArgs e)
+        {
+            AnsiConsole.MarkupLine($"[red]{(e.Error?.Message ?? "An error happened when calling the elmah.io API")}[/]");
         }
     }
 }
