fe2059e30421da890c28783fc32ab50c61da3302
Thomas Ardal <thomasardal@gmail.com>
2023-03-13 12:18:59 +0100

    Import more fields from iis and w3c log files

--- src/Elmah.Io.Cli/ImportCommand.cs
+++ src/Elmah.Io.Cli/ImportCommand.cs
@@ -4,7 +4,6 @@ using System.CommandLine;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Elmah.Io.Client;
@@ -59,18 +58,24 @@ namespace Elmah.Io.Cli
                         return;
                     }
 
-                    if (logFileType == LogFileType.w3c)
-                    {
-                        await ImportW3CFile(api, filename, logId, dateFrom, dateTo);
-                    }
-                    else if (logFileType == LogFileType.iis)
-                    {
-                        await ImportIISFile(api, filename, logId, dateFrom, dateTo);
-                    }
-                    else
-                    {
-                        AnsiConsole.MarkupLine($"[red]Unknown log file type: {logFileType}[/]");
-                    }
+                    await AnsiConsole
+                        .Status()
+                        .Spinner(Spinner.Known.Star)
+                        .StartAsync("Importing...", async ctx =>
+                        {
+                            if (logFileType == LogFileType.w3c)
+                            {
+                                await ImportW3CFile(api, filename, logId, dateFrom, dateTo);
+                            }
+                            else if (logFileType == LogFileType.iis)
+                            {
+                                await ImportIISFile(api, filename, logId, dateFrom, dateTo);
+                            }
+                            else
+                            {
+                                AnsiConsole.MarkupLine($"[red]Unknown log file type: {logFileType}[/]");
+                            }
+                    });
                 }
                 catch (Exception e)
                 {
@@ -94,21 +99,24 @@ namespace Elmah.Io.Cli
                 var message = new CreateMessage
                 {
                     ServerVariables = new List<Item>(),
+                    Data = new List<Item>(),
+                    QueryString = new List<Item>(),
+                    Cookies = new List<Item>(),
                 };
 
-                if (!string.IsNullOrWhiteSpace(line.c_ip))
+                if (!string.IsNullOrWhiteSpace(line.c_ip) && line.c_ip != "-")
                 {
-                    message.ServerVariables.Add(new Item("CLIENT-IP", line.c_ip));
+                    message.ServerVariables.Add(new Item("Client-Ip", line.c_ip));
                     parts.Add(line.c_ip);
                 }
 
-                if (!string.IsNullOrWhiteSpace(line.cs_method))
+                if (!string.IsNullOrWhiteSpace(line.cs_method) && line.cs_method != "-")
                 {
                     message.Method = line.cs_method;
                     parts.Add(line.cs_method);
                 }
 
-                if (!string.IsNullOrWhiteSpace(line.cs_uri_stem))
+                if (!string.IsNullOrWhiteSpace(line.cs_uri_stem) && line.cs_uri_stem != "-")
                 {
                     message.Url = line.cs_uri_stem;
                     parts.Add(line.cs_uri_stem);
@@ -121,9 +129,65 @@ namespace Elmah.Io.Cli
                 }
 
                 if (line.dateTime != DateTime.MinValue) message.DateTime = line.dateTime;
-                if (!string.IsNullOrWhiteSpace(line.cs_username)) message.User = line.cs_username;
-                if (!string.IsNullOrWhiteSpace(line.cs_host)) message.Hostname = line.cs_host;
-                if (!string.IsNullOrWhiteSpace(line.cs_User_Agent)) message.ServerVariables.Add(new Item("USER-AGENT", line.cs_User_Agent));
+
+                if (!string.IsNullOrWhiteSpace(line.s_computername) && line.s_computername != "-")
+                {
+                    message.Hostname = line.s_computername;
+                }
+                else if (!string.IsNullOrWhiteSpace(line.cs_host) && line.cs_host != "-")
+                {
+                    message.Hostname = line.cs_host;
+                }
+
+                if (!string.IsNullOrWhiteSpace(line.cs_host) && line.cs_host != "-")
+                {
+                    var sb = new StringBuilder();
+                    sb.Append(line.cs_host);
+                    if (!string.IsNullOrWhiteSpace(line.s_port) && line.s_port != "-")
+                    {
+                        sb.Append(':').Append(line.s_port);
+                    }
+
+                    message.ServerVariables.Add(new Item("Host", sb.ToString()));
+                }
+
+                if (!string.IsNullOrWhiteSpace(line.cs_username) && line.cs_username != "-") message.User = line.cs_username;
+                if (!string.IsNullOrWhiteSpace(line.cs_User_Agent) && line.cs_User_Agent != "-") message.ServerVariables.Add(new Item("User-Agent", line.cs_User_Agent));
+                if (!string.IsNullOrWhiteSpace(line.s_sitename) && line.s_sitename != "-") message.Application = line.s_sitename;
+                if (!string.IsNullOrWhiteSpace(line.s_ip) && line.s_ip != "-") message.ServerVariables.Add(new Item("X-Server-Ip", line.s_ip));
+                if (!string.IsNullOrWhiteSpace(line.cs_version) && line.cs_version != "-") message.ServerVariables.Add(new Item("HttpVersion", line.cs_version));
+                if (!string.IsNullOrWhiteSpace(line.cs_Referer) && line.cs_Referer != "-") message.ServerVariables.Add(new Item("Referer", line.cs_Referer));
+                if (!string.IsNullOrWhiteSpace(line.sc_substatus) && line.sc_substatus != "-") message.Data.Add(new Item("Substatus", line.sc_substatus));
+                if (!string.IsNullOrWhiteSpace(line.sc_win32_status) && line.sc_win32_status != "-") message.Data.Add(new Item("Win32 Status", line.sc_win32_status));
+                if (!string.IsNullOrWhiteSpace(line.sc_bytes) && line.sc_bytes != "-") message.Data.Add(new Item("Bytes Sent", line.sc_bytes));
+                if (!string.IsNullOrWhiteSpace(line.cs_bytes) && line.cs_bytes != "-") message.Data.Add(new Item("Bytes Received", line.cs_bytes));
+                if (!string.IsNullOrWhiteSpace(line.time_taken) && line.time_taken != "-") message.Data.Add(new Item("Time Taken", line.time_taken));
+
+                if (!string.IsNullOrWhiteSpace(line.cs_uri_query) && line.cs_uri_query != "-")
+                {
+                    var pairs = line.cs_uri_query.Split('&', StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var pair in pairs)
+                    {
+                        var keyAndValue = pair.Split('=');
+                        if (keyAndValue.Length == 2)
+                        {
+                            message.QueryString.Add(new Item(keyAndValue[0], keyAndValue[1]));
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(line.cs_Cookie) && line.cs_Cookie != "-")
+                {
+                    var pairs = line.cs_Cookie.Split(";+", StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var pair in pairs)
+                    {
+                        var keyAndValue = pair.Split('=');
+                        if (keyAndValue.Length == 2)
+                        {
+                            message.Cookies.Add(new Item(keyAndValue[0], keyAndValue[1]));
+                        }
+                    }
+                }
 
                 var title = string.Join(" - ", parts);
                 message.Title = !string.IsNullOrWhiteSpace(title) ? title : "Imported line from log file";
@@ -158,10 +222,17 @@ namespace Elmah.Io.Cli
                 var user = columns[1].Trim();
                 var date = columns[2].Trim();
                 var time = columns[3].Trim();
+                var service = columns[4].Trim(); 
                 var serverName = columns[5].Trim();
+                var serverIp = columns[6].Trim();
+                var timeTaken = columns[7].Trim();
+                var clientBytesSent = columns[8].Trim();
+                var serverBytesSent = columns[9].Trim();
                 var statusCode = columns[10].Trim();
+                var windowsStatusCode = columns[11].Trim();
                 var method = columns[12].Trim();
                 var url = columns[13].Trim();
+                var parameters = columns[14].Trim();
 
                 if (!DateTimeOffset.TryParse($"{date} {time}", CultureInfo.GetCultureInfo("en-US"), DateTimeStyles.AssumeLocal, out DateTimeOffset dateTime)) continue;
                 dateTime = dateTime.ToUniversalTime();
@@ -171,12 +242,13 @@ namespace Elmah.Io.Cli
                 {
                     DateTime = dateTime,
                     ServerVariables = new List<Item>(),
+                    Data = new List<Item>(),
                 };
                 var parts = new List<string>();
 
                 if (!string.IsNullOrWhiteSpace(clientIp) && clientIp != "-")
                 {
-                    message.ServerVariables.Add(new Item("CLIENT-IP", clientIp));
+                    message.ServerVariables.Add(new Item("Client-Ip", clientIp));
                     parts.Add(clientIp);
                 }
 
@@ -200,6 +272,13 @@ namespace Elmah.Io.Cli
 
                 if (!string.IsNullOrWhiteSpace(user) && user != "-") message.User = user;
                 if (!string.IsNullOrWhiteSpace(serverName) && serverName != "-") message.Hostname = serverName;
+                if (!string.IsNullOrWhiteSpace(service) && service != "-") message.Application = service;
+                if (!string.IsNullOrWhiteSpace(serverIp) && serverIp != "-") message.Data.Add(new Item("Server IP address", serverIp));
+                if (!string.IsNullOrWhiteSpace(timeTaken) && timeTaken != "-") message.Data.Add(new Item("Time taken", timeTaken));
+                if (!string.IsNullOrWhiteSpace(clientBytesSent) && clientBytesSent != "-") message.Data.Add(new Item("Client bytes sent", clientBytesSent));
+                if (!string.IsNullOrWhiteSpace(serverBytesSent) && serverBytesSent != "-") message.Data.Add(new Item("Server bytes sent", serverBytesSent));
+                if (!string.IsNullOrWhiteSpace(windowsStatusCode) && windowsStatusCode != "-") message.Data.Add(new Item("Windows status code", windowsStatusCode));
+                if (!string.IsNullOrWhiteSpace(parameters) && parameters != "-") message.Data.Add(new Item("Parameters", parameters));
 
                 var title = string.Join(" - ", parts);
                 message.Title = !string.IsNullOrWhiteSpace(title) ? title : "Imported line from log file";
