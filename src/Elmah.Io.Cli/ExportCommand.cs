ac4f175b161ec69d863bcaad2c3446782e151e92
Thomas Ardal <thomasardal@gmail.com>
2025-03-13 12:05:50 +0100

    Support for exporting to CSV

--- src/Elmah.Io.Cli/ExportCommand.cs
+++ src/Elmah.Io.Cli/ExportCommand.cs
@@ -1,9 +1,11 @@
-﻿using Elmah.Io.Client;
+﻿using CsvHelper;
+using Elmah.Io.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Spectre.Console;
 using System.CommandLine;
 using System.CommandLine.Binding;
+using System.Globalization;
 
 namespace Elmah.Io.Cli
 {
@@ -29,20 +31,20 @@ namespace Elmah.Io.Cli
             {
                 IsRequired = true,
             };
-            var filenameOption = new Option<string>(
-                  "--filename",
-                  getDefaultValue: () => Path.Combine(Directory.GetCurrentDirectory(), $"Export-{DateTimeOffset.Now.Ticks}.json"),
-                  "Defines the path and filename of the file to export to. Ex. \" --filename C:\\myDirectory\\myFile.json\"");
+            var filenameOption = new Option<string>("--filename", "Defines the path and filename of the file to export to. Ex. \" --filename C:\\myDirectory\\myFile.json\" or \" --filename myFile.csv\"");
             var queryOption = new Option<string>("--query", getDefaultValue: () => "*", "Defines the query that is passed to the API");
             var includeHeadersOption = new Option<bool>("--includeHeaders", "Include headers, cookies, etc. in output (will take longer to export)");
+            var formatOption = new Option<ExportFormat>("--format", getDefaultValue: () => ExportFormat.Json, "The format to export");
             var proxyHostOption = ProxyHostOption();
             var proxyPortOption = ProxyPortOption();
             var exportCommand = new Command("export", "Export log messages from a specified log")
             {
-                apiKeyOption, logIdOption, dateFromOption, dateToOption, filenameOption, queryOption, includeHeadersOption, proxyHostOption, proxyPortOption
+                apiKeyOption, logIdOption, dateFromOption, dateToOption, filenameOption, queryOption, includeHeadersOption, formatOption, proxyHostOption, proxyPortOption
             };
             exportCommand.SetHandler(async (exportModel) =>
             {
+                if (exportModel.Format == ExportFormat.Csv && exportModel.IncludeHeaders) AnsiConsole.MarkupLine("[#ffc936]Including headers is not supported when exporting to CSV[/]");
+
                 var api = Api(exportModel.ApiKey, exportModel.ProxyHost, exportModel.ProxyPort);
                 try
                 {
@@ -50,77 +52,114 @@ namespace Elmah.Io.Cli
                     if (startResult == null || startResult.Total == null || startResult.Total.Value == 0)
                     {
                         AnsiConsole.MarkupLine("[#ffc936]Could not find any messages for this API key and log ID combination[/]");
+                        return;
                     }
-                    else
-                    {
-                        int messSum = startResult.Total.Value;
 
-                        await AnsiConsole
-                            .Progress()
-                            .StartAsync(async ctx =>
+                    int messSum = startResult.Total.Value;
+
+                    if (File.Exists(exportModel.Filename)) File.Delete(exportModel.Filename);
+
+                    await AnsiConsole
+                        .Progress()
+                        .StartAsync(async ctx =>
+                        {
+                            // Define tasks
+                            var task = ctx.AddTask("Exporting log messages", new ProgressTaskSettings
                             {
-                                // Define tasks
-                                var task = ctx.AddTask("Exporting log messages", new ProgressTaskSettings
-                                {
-                                    MaxValue = messSum,
-                                });
+                                MaxValue = messSum,
+                            });
 
-                                if (File.Exists(exportModel.Filename)) File.Delete(exportModel.Filename);
-                                using (StreamWriter w = File.AppendText(exportModel.Filename))
+                            using (var w = new StreamWriter(exportModel.Filename))
+                            {
+                                string? searchAfter = null;
+                                var firstMessage = true;
+
+                                if (exportModel.Format == ExportFormat.Json) w.WriteLine("[");
+
+                                using var csv = exportModel.Format == ExportFormat.Csv ? new CsvWriter(w, CultureInfo.InvariantCulture) : null;
+                                if (csv != null)
                                 {
-                                    string? searchAfter = null;
-                                    var firstMessage = true;
-                                    w.WriteLine("[");
-                                    while (true)
+                                    csv.WriteHeader<MessageOverview>();
+                                    csv.NextRecord();
+                                }
+
+                                while (true)
+                                {
+                                    var response = await api.Messages.GetAllAsync(
+                                        exportModel.LogId.ToString(),
+                                        pageSize: 100,
+                                        query: exportModel.Query,
+                                        from: exportModel.DateFrom,
+                                        to: exportModel.DateTo,
+                                        includeHeaders: exportModel.IncludeHeaders,
+                                        searchAfter: searchAfter
+                                    );
+
+                                    if (response.Messages.Count == 0)
                                     {
-                                        var response = await api.Messages.GetAllAsync(exportModel.LogId.ToString(), pageSize: 100, query: exportModel.Query, from: exportModel.DateFrom, to: exportModel.DateTo, includeHeaders: exportModel.IncludeHeaders, searchAfter: searchAfter);
-                                        if (response.Messages.Count == 0)
-                                        {
-                                            task.Increment(task.MaxValue - task.Value);
-                                            task.StopTask();
-                                            break;
-                                        }
-                                        foreach (MessageOverview message in response.Messages)
+                                        task.Increment(task.MaxValue - task.Value);
+                                        task.StopTask();
+                                        break;
+                                    }
+
+                                    foreach (MessageOverview message in response.Messages)
+                                    {
+                                        if (exportModel.Format == ExportFormat.Json)
                                         {
                                             if (!firstMessage) w.WriteLine(",");
                                             firstMessage = false;
                                             w.WriteLine(JToken.Parse(JsonConvert.SerializeObject(message)).ToString(Formatting.Indented));
-                                            task.Increment(1);
                                         }
-                                        searchAfter = response.SearchAfter;
+                                        else if (csv != null)
+                                        {
+                                            csv.WriteRecord(message);
+                                            csv.NextRecord();
+                                        }
+
+                                        task.Increment(1);
                                     }
-                                    w.WriteLine("]");
+
+                                    searchAfter = response.SearchAfter;
                                 }
 
-                                task.StopTask();
-                            });
+                                if (exportModel.Format == ExportFormat.Json) w.WriteLine("]");
+                            }
 
-                        AnsiConsole.MarkupLine($"[green]Done with export to [/][grey]{exportModel.Filename}[/]");
-                    }
+                            task.StopTask();
+                        });
+
+                    AnsiConsole.MarkupLine($"[green]Done with export to [/][grey]{exportModel.Filename}[/]");
                 }
                 catch (Exception e)
                 {
                     AnsiConsole.MarkupLineInterpolated($"[red]{e.Message}[/]");
                 }
-            }, new ExportModelBinder(apiKeyOption, logIdOption, dateFromOption, dateToOption, filenameOption, queryOption, includeHeadersOption, proxyHostOption, proxyPortOption));
+            }, new ExportModelBinder(apiKeyOption, logIdOption, dateFromOption, dateToOption, filenameOption, queryOption, includeHeadersOption, formatOption, proxyHostOption, proxyPortOption));
 
             return exportCommand;
         }
 
-        private sealed class ExportModel(string? apiKey, Guid logId, DateTimeOffset? dateFrom, DateTimeOffset? dateTo, string? filename, string? query, bool includeHeaders, string? proxyHost, int? proxyPort)
+        private enum ExportFormat
+        {
+            Json,
+            Csv,
+        }
+
+        private sealed class ExportModel(string? apiKey, Guid logId, DateTimeOffset? dateFrom, DateTimeOffset? dateTo, string? filename, string? query, bool includeHeaders, ExportFormat format, string? proxyHost, int? proxyPort)
         {
             public string ApiKey { get; set; } = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
             public Guid LogId { get; set; } = logId;
             public DateTimeOffset? DateFrom { get; set; } = dateFrom;
             public DateTimeOffset? DateTo { get; set; } = dateTo;
-            public string Filename { get; set; } = filename ?? throw new ArgumentNullException(nameof(filename));
+            public string Filename { get; set; } = !string.IsNullOrWhiteSpace(filename) ? filename : Path.Combine(Directory.GetCurrentDirectory(), $"Export-{DateTimeOffset.Now.Ticks}.{format.ToString().ToLower()}");
             public string Query { get; set; } = query ?? throw new ArgumentNullException(nameof(query));
             public bool IncludeHeaders { get; set; } = includeHeaders;
+            public ExportFormat Format { get; set; } = format;
             public string? ProxyHost { get; set; } = proxyHost;
             public int? ProxyPort { get; set; } = proxyPort;
         }
 
-        private sealed class ExportModelBinder(Option<string> apiKeyOption, Option<Guid> logIdOption, Option<DateTimeOffset> dateFromOption, Option<DateTimeOffset> dateToOption, Option<string> filenameOption, Option<string> queryOption, Option<bool> includeHeadersOption, Option<string?> proxyHostOption, Option<int?> proxyPortOption) : BinderBase<ExportModel>
+        private sealed class ExportModelBinder(Option<string> apiKeyOption, Option<Guid> logIdOption, Option<DateTimeOffset> dateFromOption, Option<DateTimeOffset> dateToOption, Option<string> filenameOption, Option<string> queryOption, Option<bool> includeHeadersOption, Option<ExportFormat> formatOption, Option<string?> proxyHostOption, Option<int?> proxyPortOption) : BinderBase<ExportModel>
         {
             private readonly Option<string> apiKeyOption = apiKeyOption;
             private readonly Option<Guid> logIdOption = logIdOption;
@@ -129,6 +168,7 @@ namespace Elmah.Io.Cli
             private readonly Option<string> filenameOption = filenameOption;
             private readonly Option<string> queryOption = queryOption;
             private readonly Option<bool> includeHeadersOption = includeHeadersOption;
+            private readonly Option<ExportFormat> formatOption = formatOption;
             private readonly Option<string?> proxyHostOption = proxyHostOption;
             private readonly Option<int?> proxyPortOption = proxyPortOption;
 
@@ -142,6 +182,7 @@ namespace Elmah.Io.Cli
                     bindingContext.ParseResult.GetValueForOption(filenameOption),
                     bindingContext.ParseResult.GetValueForOption(queryOption),
                     bindingContext.ParseResult.GetValueForOption(includeHeadersOption),
+                    bindingContext.ParseResult.GetValueForOption(formatOption),
                     bindingContext.ParseResult.GetValueForOption(proxyHostOption),
                     bindingContext.ParseResult.GetValueForOption(proxyPortOption));
             }
